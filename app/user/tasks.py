fcf494b4d2309c4bf6a0ef07cccd6d819d740e1c
MateuszK <klusowskimat@gmail.com>
2022-09-22 21:21:40 +0200

    automate clearing expired tokens

--- app/user/tasks.py
+++ app/user/tasks.py
@@ -3,6 +3,8 @@ from celery import shared_task
 from django.core.mail import send_mail
 from django.conf import settings
 
+from oauth2_provider.models import clear_expired
+
 
 @shared_task
 def send_reset_password_email(email_subject, email_body, to_whom):
@@ -12,3 +14,8 @@ def send_reset_password_email(email_subject, email_body, to_whom):
         from_email=settings.EMAIL_HOST_USER,
         recipient_list=[to_whom]
     )
+
+
+@shared_task
+def clear_expired_tokens():
+    return clear_expired()
